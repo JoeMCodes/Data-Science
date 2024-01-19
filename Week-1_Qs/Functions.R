82ce04e69ac908b4e8512aedc9feb85b63ec6d09
Joe M Laptop <Joe.a.McGlynn@gmail.com>
2024-01-19 11:12:47 +0000

    11

--- Week-1_Qs/Functions.R
+++ Week-1_Qs/Functions.R
@@ -11,3 +11,21 @@ geometric_mean = function(x){
   n = length(x)
   return(prod(x)^(1/n))
 }
+
+
+
+#' Title
+#'
+#' @param x 1d array
+#'
+#' @return array
+#' @export
+#'
+#' @examples rolling_mean(1:7)
+#' 
+rolling_mean = function(x, window_length){
+  stopifnot(is.numeric(x))
+  n = length(x)
+  return(sapply(1:n, function(i) mean(x[1:i])))
+}
+
